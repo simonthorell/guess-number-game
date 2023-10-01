00c5098f6eac63ba63e4d1b85cdc7638a55b6e4f
Simon Thorell <kontakt@simonthorell.se>
2023-10-01 14:24:57 +0200

    Refactored input validation class using more effective method to check if input is Integer.

--- src/ValidateUserInput.java
+++ src/ValidateUserInput.java
@@ -4,25 +4,19 @@ public class ValidateUserInput {
     private final Scanner scanner = new Scanner(System.in);
 
     public int userInputAsInteger(String inputMsg, String errorMsg) {
-        String userInput;
-        int userInputAsInteger = 0;
-        boolean validInput = false;
-        
-        while (!validInput) {
+        while (true) {
             System.out.print(inputMsg);
-            userInput = scanner.nextLine();
-            
-            try {
-                // Try to convert user String input to integer.
-                userInputAsInteger = Integer.parseInt(userInput);
-                validInput = true;
 
+            try {
+                // If input is integer, loop will break and return input. 
+                return scanner.nextInt(); 
+                
             } catch (Exception e) {
-                // If user input is not an integer, print an error message.
+                // If user input is not an integer, print an error message and prompt new input.
                 System.out.println(errorMsg);
+                scanner.nextLine();
             }
         }
-        return userInputAsInteger;
     }
 
     public String userInputAsString() {
